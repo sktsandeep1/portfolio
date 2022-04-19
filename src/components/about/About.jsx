73a3b0bcb0abc717f5f830ab49732d366c2bbdec
Sandeep Kumar <63910867+sktsandeep1@users.noreply.github.com>
2022-04-19 14:37:55 +0530

    updated about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,14 +1,12 @@
-import './about.css'
-import ME from '../../assets/me-about.jpg'
-import { FaAward } from 'react-icons/fa'
-import { FaUser } from 'react-icons/fa'
-import { FaFolderOpen } from 'react-icons/fa'
-
-
+import './about.css';
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { FaFolderOpen } from 'react-icons/fa';
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
@@ -22,38 +20,36 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
-              <FaAward className='about__icon' />
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>6+ Months Working</small>
             </article>
 
             <article className="about__card">
-              <FaUser className='about__icon' />
+              <FaUser className="about__icon" />
               <h5>Clients</h5>
               <small>5+ WorldWide</small>
             </article>
 
             <article className="about__card">
-              <FaFolderOpen className='about__icon' />
+              <FaFolderOpen className="about__icon" />
               <h5>Projects</h5>
               <small>5+ Completed</small>
             </article>
           </div>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Commodi veniam quibusdam tempora corrupti, quam nam distinctio
-            recusandae illum, magni odit consequatur nihil placeat velit 
-            expedita iure at nisi modi voluptatibus.
+            Hardworking and quick learning ability having 1 year experience in
+            building front end design seeking for an opportunity to keep
+            utilizing my skills and growing with company's growth.
           </p>
 
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
-
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
