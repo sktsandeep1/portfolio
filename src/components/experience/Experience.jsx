23261dffdcbdbb3ad8248f1243af2677252063bd
Sandeep Kumar <63910867+sktsandeep1@users.noreply.github.com>
2022-04-22 17:23:10 +0530

    updated services

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,79 +1,69 @@
-import './experience.css'
-import { BsPatchCheckFill } from 'react-icons/bs'
-
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
       <div className="container experience__contianer">
-
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
-              
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>SASS</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>SASS</h4>
+                <small className="text-light">Experienced</small>
               </div>
-              
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                 <h4>BOOTSTRAP</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>BOOTSTRAP</h4>
+                <small className="text-light">Experienced</small>
               </div>
-             
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                 <h4>JAVASCRIPT</h4>
-              <small className='text-light'>Intermediate</small>
+                <h4>JAVASCRIPT</h4>
+                <small className="text-light">Intermediate</small>
               </div>
-             
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
 
               <div>
                 <h4>REACTJS</h4>
-              <small className='text-light'>Beginner</small>
+                <small className="text-light">Beginner</small>
               </div>
-              
             </article>
-
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
-
+export default Experience;
--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,23 +1,36 @@
-
-import './service.css'
-
-import React from 'react'
-
+import "./service.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 const Service = () => {
   return (
-    <section id='service'>
+    <section id="service">
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
       <div className="container service__container">
         <article className="service">
+          <BsPatchCheckFill className="experience__details-icon" />
           <div className="service__head">
-            <h3>UI/UX Design</h3>
+            <h3>Figma to Web Page</h3>
+            <small className="text-light">Html, React</small>
+          </div>
+        </article>
+        <article className="service">
+          <BsPatchCheckFill className="experience__details-icon" />
+          <div className="service__head">
+            <h3>Responsive Websites</h3>
+            <small className="text-light">Desktop, Ipad, Mobile</small>
+          </div>
+        </article>
+        <article className="service">
+          <BsPatchCheckFill className="experience__details-icon" />
+          <div className="service__head">
+            <h3>Redesign</h3>
+            <small className="text-light">Web Pages</small>
           </div>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
